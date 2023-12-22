892c0a66bada949ccea8f4b744c128f5a8305e53
Lerich Osay <lerichyaso@gmail.com>
2023-12-21 22:05:58 -0600

    started react app components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+//import packages, modules, and components
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "boostrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/Navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "/components/edit-exercise.component";
+import CreateExercise from "/components/create-exercise.component";
+import CreateUser from "/components/create-user.component";
+
+//function defines jsx for web page
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+
+      {/* provide routing for each page from root */}
+      <Route path="/" exact Component={ExercisesList}/>
+      <Route path="/edit/:id" exact Component={EditExercise}/>
+      <Route path="/create" exact Component={CreateExercise}/>
+      <Route path="/user" exact Component={CreateUser}/>
+    </Router>
   );
 }
 
